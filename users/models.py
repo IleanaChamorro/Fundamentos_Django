5187f85060d3d7130a7433408db1ba9181496e7b
ileanachamorro <ic.chamorro07@gmail.com>
2022-05-18 16:30:32 -0300

    Agregando Opciones Meta Website

--- users/models.py
+++ users/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField("El nombre de la persona" ,max_length=30)
+    last_name = models.CharField("El apellido de la persona" ,max_length=30)
+    #Nombre Verbose 
+    cars = models.ManyToManyField('Car', verbose_name='Los carros del usuario')
 
 
 STATUS_CHOICES = (
@@ -21,5 +23,12 @@ class Website(models.Model):
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['rating']
+        db_table = 'website_custom_table_name'
+        verbose_name = 'La Pagina Web'
+        verbose_name_plural = 'Las Paginas'
+
+        
 class Car(models.Model):
     name = models.CharField(max_length=40, primary_key=True)
