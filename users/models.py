ddef0eb1ca9c42928698ebf98c01abf4f1fdf50b
ileanachamorro <ic.chamorro07@gmail.com>
2022-05-18 16:44:33 -0300

    Funciones en Modelos

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class User(models.Model):
@@ -23,12 +24,28 @@ class Website(models.Model):
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ['rating']
-        db_table = 'website_custom_table_name'
-        verbose_name = 'La Pagina Web'
-        verbose_name_plural = 'Las Paginas'
+    #Fecha Publicación Post
+    def was_released_last_week(self):
+        if self.release_date < datetime.date(2022,6,6):
+            return "Released before last week"
+        else:
+            return "Released this week"
 
-        
+    #Obtener nombre del sitio
+    @property
+    def get_full_name(self):
+        return f"Este es el nombre completo del sitio web: {self.name}"
+
+
+    def __str__(self):
+        return self.name 
+
+    def get_absolute_url(self):
+        return f"/websites/{self.id}"
+    
+    def save(self, *args ,**kwargs):
+        print("Estamos guardando!")
+        super().save(*args, **kwargs)
+    
 class Car(models.Model):
     name = models.CharField(max_length=40, primary_key=True)
